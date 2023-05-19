8aae773bfc5f77bbba87e69db4cb1bfa8cac041c
San <129504601+sankim05@users.noreply.github.com>
2023-05-19 15:04:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 
 
+function setcode(elementz){
+	let arry = document.getElementsByClassName("codebox");
+Array.from(arry).forEach((item) => {
+
+    	item.style.borderWidth = "1px";
+	item.style.borderColor= "black";
+});
+
+	elementz.style.borderWidth = "5px";
+	elementz.style.borderColor= "green";
+document.getElementById("codetag").innerText = elementz.querySelector('.number').innerText + " 번 코드를 편집 중";
+	
+}
+
+document.getElementById("addit").onclick=function(){
 
 
-document.getElementById("codeaddbutton").onclick=function(){
+  let clonednode = document.getElementById("codes").insertBefore(document.getElementById("copypart").cloneNode(true) ,document.getElementById("codeaddbutton"));
+    clonednode.style.display = "flex";
+	var numberofcode = document.getElementsByClassName("codebox").length - 3;
 
-
-   var clonednode = document.getElementById("codes").insertBefore(document.getElementById("copypart").cloneNode(true) ,document.getElementById("codeaddbutton"))
-    clonednode.style.display = "flex"
+clonednode.querySelector('.number').innerText = numberofcode;
+clonednode.onclick=function(){
+	setcode(clonednode);
 
 }
+
+}
+
+document.getElementById("deleteit").onclick=function(){
+
+
+
+	let arry = document.getElementsByClassName("codebox");
+	let numberofcode = arry.length - 3;
+	
+	if(numberofcode > 0){
+		let lastelement = arry[arry.length-1];
+		lastelement.remove();
+		setcode(arry[arry.length-1]);
+	}
+
+
+
+}
+
+window.onload = function(){
+   	let arry = document.getElementsByClassName("codebox");
+let part = arry[2];
+	part.onclick=function(){
+	setcode(part);
+
+}
+}
+
