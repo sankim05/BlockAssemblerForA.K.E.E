2acc431184f6dbfb26144bc177560a89bee78e13
San <sangim261@gmail.com>
2023-05-28 01:47:22 +0900

    sum js

--- script.js
+++ script.js
@@ -46,6 +46,39 @@ document.getElementById("deleteit").onclick=function(){
 
 }
 
+function checkboxrr(){
+	let finalstring = "목적지: ";
+	let arry = document.getElementsByClassName("amongus");
+	let checkboxnum = 0;
+	Array.from(arry).forEach((item) => {
+
+		if(item.checked == true){
+			checkboxnum++;
+			if (!(checkboxnum == 1)){
+				finalstring = finalstring + ", ";
+			}
+			
+			finalstring = finalstring + item.parentElement.getElementsByTagName("span")[0].innerText
+
+		}
+
+
+});
+	if(checkboxnum==0){
+		finalstring = "목적지 없음";
+	}
+	
+	document.getElementById("00000").innerText = finalstring;
+	if (document.getElementById("00000").innerText.length > 20){
+		document.getElementById("00000").style.fontSize = "20px";
+	}else if(document.getElementById("00000").innerText.length > 13){
+		document.getElementById("00000").style.fontSize = "25px";
+	}else{
+		document.getElementById("00000").style.fontSize = "30px";
+	}
+}
+
+
 window.onload = function(){
    	let arry = document.getElementsByClassName("codebox");
 let part = arry[2];
@@ -53,5 +86,7 @@ let part = arry[2];
 	setcode(part);
 
 }
+
+
 }
 
